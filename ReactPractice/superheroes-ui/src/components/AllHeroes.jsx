3946ef7d8999f8506d9762d082b5462d8e042ac9
Vlad <lutavlad@yahoo.com>
2023-07-31 16:11:49 +0100

    React works!

--- ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
+++ ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroDetail from './HeroDetail'
+import Row from 'react-bootstrap/Row'
 
 const AllHeroes = () => {
 
@@ -16,7 +17,7 @@ const AllHeroes = () => {
         { "id": 10, "name": "Dick Grayson", "alias": "Nightwing", "superpower": "Acrobatics", "teamID": 5 }
     ]
     return (
-        <Row >
+        <Row>
           {heroes.map(hero => (
               <div className='container' key={hero.id}>
                 <HeroDetail info={hero}/>
